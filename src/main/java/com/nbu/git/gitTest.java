5772944ba847a1bb732abf674f38ba3bd96ad510
Pure <876338417@qq.com>
2022-06-23 13:07:46 +0800

    second commit

--- src/main/java/com/nbu/git/gitTest.java
+++ src/main/java/com/nbu/git/gitTest.java
@@ -3,5 +3,6 @@ package com.nbu.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
